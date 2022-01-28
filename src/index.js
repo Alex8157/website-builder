d9c770855c1602ad9fbbe8753414dd54061d19dc
Lyosha <alex.4618157.com>
2022-01-28 15:22:01 +0700

    some functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const body = document.getElementsByTagName('body');
-let counter = 0;
 
 class ButtonFactory {
   create() {
@@ -10,11 +9,10 @@ class ButtonFactory {
 }
 
 class VirtualNode {
-  constructor(type, button) {
+  constructor(type) {
     this.type = type;
     this.nodes = [];
     this.DOMElement = document.createElement(type);
-    this.DOMElement.appendChild(button);
   }
   addNode(node) {
     this.nodes.push(node);
@@ -25,49 +23,24 @@ class VirtualNode {
   }
 }
 
-const buttonFactory = new ButtonFactory();
-
-const root = new VirtualNode('div', buttonFactory.create());
-const v1 = new VirtualNode('div', buttonFactory.create());
-const v2 = new VirtualNode('div', buttonFactory.create());
-root.addNode(v1);
-root.addNode(v2);
-
-body[0].appendChild(root.getDOMElement());
-
-function addNode(id = counter) {
-  if (!root) {
-    root = new VirtualNode('div', counter);
-    counter++;
-  } else {
-    const node = findNode(root, id);
-    node.addNode(new VirtualNode('div'));
-  }
-  body[0].innerHTML = makeInterface(root);
-}
-
-function findNode(node, id) {
-  if (node.id === id) {
+class VirtualNodeFactory {
+  create(type) {
+    const node = new VirtualNode(type);
+    const buttonFactory = new ButtonFactory();
+    const button = buttonFactory.create();
+    button.addEventListener('click', () => {
+      node.addNode(this.create('div'));
+    });
+    node.getDOMElement().appendChild(button);
     return node;
   }
-  for (let i = 0; i < node.nodes.length; i++) {
-    const result = findNode(node.nodes[i], id);
-    if (result) {
-      return result;
-    }
-  }
 }
 
-function makeInterface(node) {
-  let html = '';
-  if (node.nodes.length > 0) {
-    html = `<${node.type} id=${node.id}>`;
-    for (let i = 0; i < node.nodes.length; i++) {
-      html += `${makeInterface(node.nodes[i])}`;
-    }
-    html += `</${node.type}>`;
-  } else {
-    html = `<${node.type}><button onclick="addNode(${node.id})">Добавить</button></${node.type}>`;
-  }
-  return html;
+document.addEventListener('DOMContentLoaded', start());
+
+function start() {
+  const nodeFactory = new VirtualNodeFactory();
+  const root = nodeFactory.create('div');
+
+  body[0].appendChild(root.getDOMElement());
 }
